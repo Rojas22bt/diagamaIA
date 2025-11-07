34c298cf7bedc3d40dc254c192f2eacb700b6025
Bot20rp <d.sajor2003rp@gmail.com>
2025-11-06 20:13:13 -0400

    prueba

--- diagramadoria/src/api/axiosInstance.ts
+++ diagramadoria/src/api/axiosInstance.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 const axiosInstance = axios.create({
  baseURL: 'http://localhost:3000/api',
   //baseURL: 'https://diagamaia.onrender.com/api', 
+  //hola
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
