0b6d5405a381fa6b48ddd7dd93ec13996134ecb6
BoteritoROOT <d.sajor2003rp@gmail.com>
2025-11-11 18:41:06 -0400

    urls

--- diagramadoria/src/api/axiosInstance.ts
+++ diagramadoria/src/api/axiosInstance.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
- baseURL: 'http://localhost:3000/api',
-  // baseURL: 'https://diagamaia.onrender.com/api',
+//  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://diagamaia.onrender.com/api',
   //hola
   timeout: 10000,
   headers: {
